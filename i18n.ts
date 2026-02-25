ee77301ec496bd4f7b97e7edd88bf52707aa3bb6
Aleksandr Kolesov <vvruspat@gmail.com>
2026-02-25 09:40:39 +0100

    locales

--- i18n.ts
+++ i18n.ts
@@ -2,7 +2,12 @@ import * as Localization from "expo-localization";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import de from "./locales/de.json";
+import el from "./locales/el.json";
 import en from "./locales/en.json";
+import es from "./locales/es.json";
+import fr from "./locales/fr.json";
+import it from "./locales/it.json";
 import nl from "./locales/nl.json";
 import ru from "./locales/ru.json";
 
@@ -14,7 +19,12 @@ i18n.use(initReactI18next).init({
 	lng: deviceLanguage,
 	fallbackLng: "en",
 	resources: {
+		de: { translation: de },
+		el: { translation: el },
 		en: { translation: en },
+		es: { translation: es },
+		fr: { translation: fr },
+		it: { translation: it },
 		nl: { translation: nl },
 		ru: { translation: ru },
 	},
